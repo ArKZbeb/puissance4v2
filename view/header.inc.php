24d20dbfafc7052aa6189b94d3065003b5af382b
ArKZbeb <116552625+ArKZbeb@users.noreply.github.com>
2022-11-17 10:19:58 +0100

    commit

--- view/header.inc.php
+++ view/header.inc.php
@@ -6,6 +6,15 @@
             <li><a href="register.php">JEU</a></li>
             <li><a href="scores.php">SCORES</a></li>
             <li><a href="contact.php">NOUS CONTACTER</a></li>
+
+            <?php
+            if (isset($_SESSION['id'])) {
+                echo '<li><a href="profile.php"><img src="assets/images/profile.png" alt="Profile picture" /></a></li>';
+            } else {
+                echo '<li><a href="login.php">SE CONNECTER</a></li>';
+            }
+            ?>
+            
         </ul>
     </nav>
 </header>
