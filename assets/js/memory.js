9f3138b7b13ca7ea40b9161b3f984e92dc04d45a
SiERRA74 <75257452+SiERRA74@users.noreply.github.com>
2022-11-24 22:11:46 +0100

    Update memory.js

--- assets/js/memory.js
+++ assets/js/memory.js
@@ -240,7 +240,7 @@ function checkStrength(password) {
 /* -------------------------------------------------------------------------- */
 /*                                    Timer                                   */
 /* -------------------------------------------------------------------------- */
-let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let [milliseconds, seconds, minutes] = [0, 0, 0];
 let timer = document.querySelector(".timer");
 let int = null;
 
@@ -254,14 +254,9 @@ function updateTimer() {
         if (seconds >= 60) {
             seconds = 0;
             minutes++;
-
-            if (minutes >= 60) {
-                minutes = 0;
-                hours++;
-            }
         }
     }
-    let h = hours < 10 ? "0" + hours : hours;
+    
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
     let ms =
@@ -271,7 +266,7 @@ function updateTimer() {
             ? "0" + milliseconds
             : milliseconds;
 
-    timer.innerHTML = ` ${h} : ${m} : ${s} : ${ms} `;
+    timer.innerHTML = ` ${m} : ${s} : ${ms} `;
 }
 
 function startGame() {
